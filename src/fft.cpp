77a483f215e84c0a892b86aa62dadc51a720cd46
Danil Osherov <osherov.danil@gmail.com>
2013-06-23 19:11:01 +0000

    Fast Fourier Transform

---
+++ src/fft.cpp
@@ -0,0 +1,29 @@
+typedef complex<double> base;
+
+void fft (vector<base> & a, bool invert) {
+	int n = (int) a.size();
+
+	for (int i = 1, j = 0; i < n; i++) {
+		int bit = n >> 1;
+		for (; j >= bit; bit >>= 1)
+			j -= bit;
+		j += bit;
+		if (i < j) swap (a[i], a[j]);
+	}
+
+	for (int len = 2; len <= n; len <<= 1) {
+		double ang = 2 * PI/len * (invert ? -1 : 1);
+		base wlen (cos(ang), sin(ang));
+		for (int i = 0; i < n; i += len) {
+			base w (1);
+			for (int j = 0; j < len/2; j++) {
+				base u = a[i+j], v = a[i+j+len/2] * w;
+				a[i+j] = u + v;
+				a[i+j+len/2] = u - v;
+				w *= wlen;
+			}
+		}
+	}
+	if (invert)
+		REP(i, 0, n) a[i] /= n;
+}
