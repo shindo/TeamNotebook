a801aece14bf80bfab0b4831ea84d22cb3819454
Danil Osherov <osherov.danil@gmail.com>
2013-06-23 22:07:33 +0000

    minor changes

--- src/dinic.cpp
+++ src/dinic.cpp
@@ -6,7 +6,7 @@ bool bfs(int src, int dest) {
     q[H ++] = src;
     REP(i, 0, H) {
         int cur = q[i];
-        for(int e = head[cur]; e >= 0; e = nxt[e]) {
+        for(int e = head[cur]; e != -1; e = nxt[e]) {
             if (capa[e] > flow[e] && dist[to[e]] == -1) {
                 dist[to[e]] = dist[cur] + 1;
                 q[H ++] = to[e];
@@ -19,7 +19,7 @@ bool bfs(int src, int dest) {
 int dfs(int cur, int curflow) {
     if (cur == dest) return curflow;
     int d ;
-    for(int& e = work[cur]; e >= 0; e = nxt[e]) {
+    for(int& e = work[cur]; e != -1; e = nxt[e]) {
         if (capa[e] > flow[e] && (dist[to[e]] == dist[cur] + 1) &&
                 (d = dfs(to[e], min(curflow, capa[e] - flow[e])))) {
             flow[e] += d;
