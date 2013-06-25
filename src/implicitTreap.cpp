d069fdb5fb0da3f5a5b7f37d8f4e077983e3446d
Danil Osherov <osherov.danil@gmail.com>
2013-06-25 23:09:50 +0000

    Treap

---
+++ src/implicitTreap.cpp
@@ -0,0 +1,29 @@
+int cnt (pitem t) {
+	return t ? t->cnt : 0;
+}
+
+void upd_cnt (pitem t) {
+	if (t)
+		t->cnt = 1 + cnt(t->l) + cnt (t->r);
+}
+
+void merge (pitem & t, pitem l, pitem r) {
+	if (!l || !r)
+		t = l ? l : r;
+	else if (l->prior > r->prior)
+		merge (l->r, l->r, r),  t = l;
+	else
+		merge (r->l, l, r->l),  t = r;
+	upd_cnt (t);
+}
+
+void split (pitem t, pitem & l, pitem & r, int key, int add = 0) {
+	if (!t)
+		return void( l = r = 0 );
+	int cur_key = add + cnt(t->l);
+	if (key <= cur_key)
+		split (t->l, l, t->l, key, add),  r = t;
+	else
+		split (t->r, t->r, r, key, add + 1 + cnt(t->l)),  l = t;
+	upd_cnt (t);
+}
---
+++ src/treap.cpp
@@ -0,0 +1,34 @@
+struct item {
+	int key, prior;
+	item * l, * r;
+	item() { }
+	item (int key, int prior) : key(key), prior(prior), l(NULL), r(NULL) { }
+};
+typedef item * pitem;
+
+void split (pitem t, int key, pitem & l, pitem & r) {
+	if (!t)
+		l = r = NULL;
+	else if (key < t->key)
+		split (t->l, key, l, t->l),  r = t;
+	else
+		split (t->r, key, t->r, r),  l = t;
+}
+
+void insert (pitem & t, pitem it) {
+	if (!t)
+		t = it;
+	else if (it->prior > t->prior)
+		split (t, it->key, it->l, it->r),  t = it;
+	else
+		insert (it->key < t->key ? t->l : t->r, it);
+}
+
+void merge (pitem & t, pitem l, pitem r) {
+	if (!l || !r)
+		t = l ? l : r;
+	else if (l->prior > r->prior)
+		merge (l->r, l->r, r),  t = l;
+	else
+		merge (r->l, l, r->l),  t = r;
+}
